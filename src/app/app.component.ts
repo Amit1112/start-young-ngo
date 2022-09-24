56ff8d374e9e19d307b71a1608ef027917051cbf
Amit Vishwakarma <amitv094@gmail.com>
2022-09-24 19:02:06 +0530

    Added initial Angular code with some UI changes and SEO impl

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'start-young-uk';
+
+  constructor(
+    private titleService: Title,
+    private metaTagService: Meta) { }
+
+  ngOnInit() {
+    this.titleService.setTitle("Start Young UK");
+    this.metaTagService.addTags([
+      {
+        name: 'keywords',
+        content: 'Start Young UK, Charity Start Young UK, Start Young UK NGO, UK best NGO, best ngo, best charity, top charities UK, young uk',
+      },
+      { name: 'description', content: 'Start Young Uk - Start Young UK' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Start Young UK' },
+    ]);
+  }
+}
